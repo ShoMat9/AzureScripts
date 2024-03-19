ccdef89d78381c786abc24763e72b9101276d742
Shobhit Mathur <shobhitmathur18@gmail.com>
2024-03-19 11:25:19 +1100

    Update CopyVMTags.ps1

--- CopyVMTags.ps1
+++ CopyVMTags.ps1
@@ -1,7 +1,10 @@
-Set-AzContext -SubscriptionName sub
+## You know what to edit!!
 
+$SubscriptionName = "sub1"  
 $SourceVM = "abc"
 $DestinationVM = "xyz"
+
+Set-AzContext -SubscriptionName $SubscriptionName
 $SVM = Get-AzVM -name $SourceVM
 $sourceTags = ($SVM).Tags
 $DVM = Get-AzVM -name $DestinationVM
