ad5e35e5712405564f6ab1af482cdab28e83a5cb
Shobhit Mathur <shobhitmathur18@gmail.com>
2024-03-28 12:35:41 +1100

    IP, NIC, Subnet, NSG, ASG details for multiple VMs

--- VM-NetworkInfo.ps1
+++ VM-NetworkInfo.ps1
@@ -1,23 +1,28 @@
-## You know what to edit :)
+# You know what to edit :)
 
-$resourceGroupName = "RG Name"
-$vmName = "VM1","VM2","VM3"
+$subscriptionName = sub1
+$vmNames = "abc", "pqr", "xyz"
 $ShoReport = Get-Date -UFormat "C:\Reports\VM-NSG-x-%Y%b%d@%I%M%p.html"
 
-######################################################
+#######################################################
+Select-AzSubscription  $subscriptionName
+
 $ShoStyle = "<style>
 TABLE {border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;Font-Size: 8pt;Font-Family: Tahoma, sans-serif;}
 TH {border-width: 1px;padding: 3px;border-style: solid;border-color: black;background-color: #bc64ed;}
 TD {border-width: 1px;padding: 3px;border-style: solid;border-color: black;}
 </style>"
 
-foreach ($vm1 in $vmName)
+foreach ($vm in $vmName)
 {   
-$report +=  Get-AzVM -ResourceGroupName $resourceGroupName -Name $vm1 | Select Name,
-@{N='NIC';E={$_.NetworkProfile.NetworkInterfaces.ID.Split('/')[-1]}}, 
+$vmx = Get-AzVM -Name $vm
+$resourceGroupName = $vmx.ResourceGroupName
+$report +=  Get-AzVM -ResourceGroupName $ResourceGroupName -Name $vm | Select Name,
 @{N='IP';E={(Get-AzNetworkInterface -Name $_.NetworkProfile.NetworkInterfaces.ID.Split('/')[-1]).IpConfigurations.PrivateIpAddress.Split('/')[-1]}},
+@{N='NIC';E={$_.NetworkProfile.NetworkInterfaces.ID.Split('/')[-1]}}, 
+@{N='Subnet';E={(Get-AzNetworkInterface -Name $_.NetworkProfile.NetworkInterfaces[0].Id.Split('/')[-1]).IpConfigurations[0].Subnet.Id.split('/')[-1]}},
 @{N='NSG';E={(Get-AzNetworkInterface -Name $_.NetworkProfile.NetworkInterfaces.ID.Split('/')[-1]).NetworkSecurityGroup.Id.Split('/')[-1]}},
-@{N='subnetName';E={(Get-AzNetworkInterface -Name $_.NetworkProfile.NetworkInterfaces[0].Id.Split('/')[-1]).IpConfigurations[0].Subnet.Id.split('/')[-1]}} 
+@{N='ASG';E={(Get-AzNetworkInterface -Name $_.NetworkProfile.NetworkInterfaces.ID.Split('/')[-1]).IpConfigurations.ApplicationSecurityGroups.Id.Split('/')[-1]}}
 }
 
 $report | ConvertTo-Html -head $ShoStyle | out-file $ShoReport
