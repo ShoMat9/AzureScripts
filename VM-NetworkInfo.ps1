5800506ed7c715e4ce0d59aca46c398499de6063
Shobhit Mathur <shobhitmathur18@gmail.com>
2024-06-26 11:51:37 +1000

    Update VM-NetworkInfo.ps1

--- VM-NetworkInfo.ps1
+++ VM-NetworkInfo.ps1
@@ -27,3 +27,33 @@ $report +=  Get-AzVM -ResourceGroupName $ResourceGroupName -Name $vm | Select Na
 
 $report | ConvertTo-Html -head $ShoStyle | out-file $ShoReport
 ######################################################
+
+$csvData = Import-Csv -Path "C:\X-Sho\Scripts\CSV\vms-csv.csv"
+$vmData = @()
+
+foreach ($row in $csvData) {
+    $currentSubscription = (Get-AzContext).Subscription.Name
+    $subscriptionname = $row.SUBSCRIPTION
+
+    if ($subscriptionname -ne $currentSubscription) {
+        Set-AzContext -SubscriptionName $subscriptionname
+        $currentSubscription = $subscriptionname
+    }
+
+    $resourceGroupName = $row.'RESOURCEGROUP'
+    $vmName = $row.NAME
+
+    $vmInfo = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName | Select-Object Name,
+    @{N='IP'; E={(Get-AzNetworkInterface -Name $_.NetworkProfile.NetworkInterfaces.ID.Split('/')[-1]).IpConfigurations.PrivateIpAddress.Split('/')[-1]}},
+    @{N='NIC'; E={$_.NetworkProfile.NetworkInterfaces.ID.Split('/')[-1]}},
+    @{N='Subnet'; E={(Get-AzNetworkInterface -Name $_.NetworkProfile.NetworkInterfaces[0].Id.Split('/')[-1]).IpConfigurations[0].Subnet.Id.Split('/')[-1]}},
+    @{N='NSG'; E={(Get-AzNetworkInterface -Name $_.NetworkProfile.NetworkInterfaces.ID.Split('/')[-1]).NetworkSecurityGroup.Id.Split('/')[-1]}},
+    @{N='ASG'; E={(Get-AzNetworkInterface -Name $_.NetworkProfile.NetworkInterfaces.ID.Split('/')[-1]).IpConfigurations.ApplicationSecurityGroups.Id.Split('/')[-1]}}
+
+    $vmData += $vmInfo
+}
+
+$htmlContent = $vmData | ConvertTo-Html -head $ShoStyle -Property Name, IP, NIC, Subnet, NSG, ASG 
+$htmlContent | Out-File $ShoReport
+######################################################
+
